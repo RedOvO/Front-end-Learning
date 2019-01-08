2db030391a819ec46d341ba5a03e380d25773925
RedOvO <599236422@qq.com>
2019-01-09 00:01:21 +0800

    @learn js

--- app.js
+++ app.js
@@ -5,3 +5,13 @@ var author = {
     name: 'john',
     age: 23
 }
+var x = 3, y, z;
+y = x++;
+z = ++x;
+console.log(y + "&" + z);
+function test(num){
+    num = num || 100;
+    console.log(num);
+}
+test(20);
+test()
