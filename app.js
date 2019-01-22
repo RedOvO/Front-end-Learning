f7304774c1ab5a4e02a7d5d221553aa95049f8e1
RedOvO <599236422@qq.com>
2019-01-22 09:41:04 +0800

    @learn js date

--- app.js
+++ app.js
@@ -28,6 +28,33 @@ var obj = {
 };
 obj.say('welcome');
 
+var phone = {};
+        console.log(phone.toString());
+
 var num = 10.005;
 console.log(num.toFixed(2));
 console.log(Math.random());
+
+var obj = {
+    0:1,
+    1:6,
+    2:3,
+    length:3,
+};
+var array = Array.from(obj);
+console.log(array);
+
+var date1 = new Date(2016, 11, 11, 11, 11 ,11);
+console.log(date1);
+var date2 = new Date(2016, 12, 11, 11, 11 ,11);
+console.log(date2)
+var date = new Date();
+var year = date.getFullYear();
+var month = date.getMonth();
+var day = date.getDate();
+var hour = date.getHours();
+var minute = date.getMinutes();
+var second = date.getSeconds();
+var time = date.getTime();
+var week = date.getDay();
+console.log(year, month, day, hour, minute, second, time, week);
