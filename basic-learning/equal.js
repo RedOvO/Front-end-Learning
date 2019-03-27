ec532f25b5aee33cca9e4034a8632dacaeeda17e
RedOvO <599236422@qq.com>
2019-03-27 19:15:20 +0800

    modify equal.js

--- basic-learning/equal.js
+++ basic-learning/equal.js
@@ -2,21 +2,18 @@ const compare = (obj1, obj2) => {
 	var type1 = typeof obj1;
 	var type2 = typeof obj2;
 	if (type1 !== type2) {
-			return false;
+		return false;
 	} else if (type1 === "object") {
-			var array1 = Object.keys(obj1);
-			var array2 = Object.keys(obj2);
-			if(array1.length !== array2.length){
-					return false;
-			}
-			for(var i = 0; i < array1.length; i++){
-					if(obj1.array1[i] !== obj2.array2[i]) {
-							return false;
-					}
-			}
-			return true;
+		var array1 = Object.keys(obj1);
+		var array2 = Object.keys(obj2);
+		if (array1.length !== array2.length) {
+			return false;
+		}
+		return array1.every((item) => {
+			return obj1[item] === obj2[item];
+		})
 	} else {
-			return obj1 === obj2;
+		return obj1 === obj2;
 	}
 }
 
@@ -25,8 +22,15 @@ const compare = (obj1, obj2) => {
 const cases = [];
 // case1 
 cases.push([
-	{},
-	{},
+	{
+		a: 1,
+		b: 2
+	},
+	{
+		c: 1,
+		d: 2
+	},
+
 ]);
 // case2
 cases.push([
